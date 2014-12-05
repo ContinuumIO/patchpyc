14f0f042b514d1de750ffe995d08829446ebc148
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-12-05 13:54:28 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="patchpyc",
+    description="Patch PYC file",
+    author="Continuum Analytics, Inc.",
+    license="BSD",
+    py_modules=['patchpyc'],
+)
